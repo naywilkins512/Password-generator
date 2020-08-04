3f301e33e5819579e19eb28a438958f1c07125c9
naywilkins512 <nathanwilkins01@gmail.com>
2020-08-03 22:43:11 -0500

    commented out syntax

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,12 @@ const generateBtn = document.querySelector("#generate");
 
 
 
-const randomFunction = {
-    numb: getRandomNumber,
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    special: getRandomSymbol
-};
+// const collection = {
+//     numb: getRandomNumber,
+//     lower: getRandomLower,
+//     upper: getRandomUpper,
+//     special: getRandomSymbol
+// };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", makePassword)
@@ -18,7 +18,7 @@ generateBtn.addEventListener("click", makePassword)
 //  pass query selector to password generating questions
 
 function makePassword() {
-    char = parseInt(prompt("how many characters do you want in your password? choose between 8 and 30")) 
+   var char = parseInt(prompt("how many characters do you want in your password? choose between 8 and 30")) 
         if (!char) {
             alert("you have to enter something!")
         }
@@ -26,25 +26,29 @@ function makePassword() {
             alert("pick between 8 and 30")
         }
         else if (char >= 8 || char <= 30) { 
-        numb = confirm("do you want numbers?")
-        upper = confirm("do you want uppercase?")
-        lower = confirm("do you want lowercase?")
-        special = confirm("do you want special characters?")
+    var numb = confirm("do you want numbers?")
+    var upper = confirm("do you want uppercase?")
+    var lower = confirm("do you want lowercase?")
+    var special = confirm("do you want special characters?")
  
         }
-    //  if char === true || numb === true || upper === true || lower === true || special === true 
-     }
-
+   numbersGenerator(char, numb, upper, lower, special)
+    };
+ 
+     
+ function numbersGenerator(char, numb, uppper, lower, special) {
+    return
+ }
     //  1. pass what user said yes to to generator functions ... maybe if === true statements?
     //  2. pass generator results to password output box
 
-
-   
+  
 //   pass what user entered to the Generator Functions
 
 
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+
 }
 
 function getRandomLower() {
